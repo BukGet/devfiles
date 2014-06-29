79b049fdba4f47f3489c0a6e396679c34b30eb07
Steven McGrath <steve@chigeek.com>
2014-06-29 18:53:48 -0500

    Removed the @tasks as these are always called from within other scripts.

--- fabfile/common.py
+++ fabfile/common.py
@@ -1,20 +1,25 @@
 from fabric.api import *
 from fabric.contrib import *
 
-@task
+
 def installed(package):
     env.warn_only = True        # Just setting this incase we need it.
     return not 'is not installed' in run('rpm -q %s' % package)
 
 
-@task
-def yum(*packages, action='install'):
+def yum(action='install', *packages):
     return run('yum -y %s %s' % (action, ' '.join(packages)))
 
 
-@task
 def dl_template(template, location):
     raw_file_host = 'https://raw.githubusercontent.com'
     tmpl_base = '/BukGet/devfiles/master/templates'
     url = '%s%s/%s' % (raw_file_host, tmpl_base, template)
     run('curl -o %s %s' % (location, url))
+
+
+def service(name, action, mech='service'):
+    if mech == 'service':
+        run('service %s %s' % (name, action))
+    elif mech == 'initctl':
+        run('initctl %s %s' % (action, name))
