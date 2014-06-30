f58f4d095c417e58fe737aa82e291907d46afd92
Steven McGrath <steve@chigeek.com>
2014-06-29 19:52:00 -0500

    Now the generators can has ssh

--- fabfile/production.py
+++ fabfile/production.py
@@ -2,7 +2,7 @@ from fabric.api import *
 from fabric.contrib import *
 
 @task
-def promote():
+def promote(server='api'):
     '''
     Promotes a server to production status.
     '''
@@ -12,9 +12,7 @@ def promote():
     if 'Firewall is not running' in run('service iptables status'):
         run('service iptables start')
 
-    # We need to adjust the IPTables rules to block communication on
-    # non-essential ports, restrict SSH communication from the outside world,
-    # and generally just lock things down a touch.
+
     run('iptables -F INPUT')
     run('iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT')
     run('iptables -A INPUT -p icmp -j ACCEPT')
@@ -26,13 +24,18 @@ def promote():
 
     # Also allow SSH from the bukget.org webhost.  This is a failsafe incase
     # Hamachi is down & we have issues with the API servers.
-    run('iptables -A INPUT -p tcp -m state --state NEW -m tcp --dport 22 --src 70.32.35.194 -j ACCEPT')
+    if server == 'api':
+        run('iptables -A INPUT -p tcp -m state --state NEW -m tcp --dport 22 --src 70.32.35.194 -j ACCEPT')
+    if server == 'gen':
+        run('iptables -A INPUT -p tcp -m state --state NEW -m tcp --dport 22 -j ACCEPT')
 
     run('iptables -A INPUT -p tcp -m state --state NEW,ESTABLISHED -m tcp --dport 53 -j ACCEPT')
     run('iptables -A INPUT -p udp -m state --state NEW,ESTABLISHED -m udp --dport 53 -j ACCEPT')
 
     # We also kinda need to enable port 80 for all the web traffic we expect ;)
-    run('iptables -A INPUT -p tcp -m state --state NEW -m tcp --dport 80 -j ACCEPT')
+    if server == 'api':
+        run('iptables -A INPUT -p tcp -m state --state NEW -m tcp --dport 80 -j ACCEPT')
+        run('iptables -A INPUT -p tcp -m state --state NEW -m tcp --dport 443 -j ACCEPT')
 
     # Lastly, lets finish this off and save the iptables config.
     run('iptables -A INPUT -j REJECT --reject-with icmp-host-prohibited')
